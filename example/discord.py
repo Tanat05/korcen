4fce155304826b6d5dd5f81ef13f2d797d99d287
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-04-07 00:16:59 +0900

    Update discord.py

--- example/discord.py
+++ example/discord.py
@@ -1,9 +1,10 @@
 import discord
 from discord.ext import commads
-from korcen import check
+from korcen import korcen
 
 TOKEN = "봇 토큰 넣기"
 bot = commands.Bot(command_prefix = "!")
+korcen = korcen.korcen()
 
 
 @bot.event
@@ -15,21 +16,17 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    a = check.check(message.content)
-    if a != 0:
-    #비속어 종류에 따라 1~9 없다면 0
+    a = korcen.check(message.content)
+    if a == True:
+    #비속어가 있다면 True 없다면 False
         await message.delete()
         #메세지 삭제
         embed = discord.Embed(title = "메세지 삭제함", description = "")
         embed.set_footer(text = "디스코드 TNS 봇")
         #모듈 사용 여부 표기하기
-        #봇 소개,도움,검열 메세지 등 유저가 볼수있는 원하는 곳에 모듈 사용 표기
+        #봇 소개,도움,검열 메세지 등 유저가 볼수있는 곳중 원하는 곳에 모듈 사용 표기
     await bot.process_commads(msg)
     
-    
-@bot.command()
-async def ping(ctx):
-    await ctx.send(f"{round(bot.latency * 1000)}ms")
         
         
 bot.run(TOKEN)
