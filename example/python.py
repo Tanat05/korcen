301be6d4be9a8632213db42982f25037d5b15382
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-04-09 00:17:54 +0900

    Update python.py

--- example/python.py
+++ example/python.py
@@ -1,4 +1,3 @@
-#0.0.5
 from korcen import korcen
 
 korcen = korcen.korcen()
@@ -6,9 +5,8 @@ korcen = korcen.korcen()
 text = input()
 a = korcen.check(text)
 print(a)
-#비속어가 있는지 없는지 확인
-#0.0.3 0 ~ 9 출력
-#0.0.5 True 또는 False 출력
+#모든 비속어
+#True 또는 False 출력
 
 a = korcen.general(text)
 print(a)
