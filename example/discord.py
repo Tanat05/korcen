6cc82828bd0a819ff03ca26d8b6c5e897dcf3c1f
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-04-12 22:07:18 +0900

    Update discord.py

--- example/discord.py
+++ example/discord.py
@@ -16,15 +16,10 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    a = korcen.check(message.content)
-    if a == True:
-    #비속어가 있다면 True 없다면 False
+    if korcen.check(message.content):
         await message.delete()
-        #메세지 삭제
         embed = discord.Embed(title = "메세지 삭제함", description = "")
         embed.set_footer(text = "디스코드 TNS 봇")
-        #모듈 사용 여부 표기하기
-        #봇 소개,도움,검열 메세지 등 유저가 볼수있는 곳중 원하는 곳에 모듈 사용 표기
     await bot.process_commads(msg)
     
         
