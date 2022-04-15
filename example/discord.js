e262ce794f7da31e5301c19356e80f5ef9d5c86b
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-04-15 10:15:04 +0900

    Update discord.js

--- example/discord.js
+++ example/discord.js
@@ -1,1 +1,19 @@
-준비중
+const { check } = require("korcen");
+const { MessageEmbed } = require("discord.js");
+
+client.on("messageCreate", async (message) => {
+  if (message.author.id === "") return; // 특정 유저 검사 무시
+  const content = message.content;
+  const c = check(content);
+
+  if (c) {
+    message.delete();
+    message.channel.send({
+      embeds: [
+        new MessageEmbed()
+          .setTitle("메세지 삭제함")
+          .setFooter({ text: "디스코드 TNS 봇" }),
+      ],
+    });
+  }
+});
