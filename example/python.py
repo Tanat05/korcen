39de3b6c77dd535d0a733d2fbc86718c81bcd391
Tanat <85154556+Tanat05@users.noreply.github.com>
2022-05-01 22:50:12 +0900

    Update python.py

--- example/python.py
+++ example/python.py
@@ -3,55 +3,46 @@ from korcen import korcen
 korcen = korcen.korcen()
 
 text = input()
+
 a = korcen.check(text)
 print(a)
 #모든 비속어
-#True 또는 False 출력
 
 a = korcen.general(text)
 print(a)
 #일반 욕설
-#True 또는 False 출력
 
 a = korcen.minor(text)
 print(a)
 #사소한 욕설
-#True 또는 False 출력
 
 a = korcen.sexual(text)
 print(a)
 #성적발언
-#True 또는 False 출력
 
 a = korcen.belittle(text)
 print(a)
 #비하발언
-#True 또는 False 출력
 
 a = korcen.race(text)
 print(a)
 #인종차별발언
-#True 또는 False 출력
 
 a = korcen.parent(text)
 print(a)
 #패드립
-#True 또는 False 출력
 
 a = korcen.foreign(text)
 print(a)
 #외국 욕설
-#True 또는 False 출력
 
 a = korcen.special(text)
 print(a)
 #특수(이모지)
-#True 또는 False 출력
 
 a = korcen.politics(text)
 print(a)
 #정치발언
-#True 또는 False 출력
 
 
 
